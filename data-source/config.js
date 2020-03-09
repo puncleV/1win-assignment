d68393896483ac60992a4902baaabb887e8f497a
punkkk <punkrus18@gmail.com>
2020-03-09 13:48:58 +0300

    add: default value for rabbit host

--- data-source/config.js
+++ data-source/config.js
@@ -1,7 +1,7 @@
 export const config = {
   rabbit: {
     PROTOCOL: 'amqp',
-    HOST: process.env.RABBIT_HOST,
+    HOST: process.env.RABBIT_HOST || 'localhost',
     PORT: 5672,
     ODDS_QUEUE_NAME: 'odds',
   },
